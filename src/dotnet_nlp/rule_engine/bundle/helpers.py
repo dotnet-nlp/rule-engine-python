50995234b992e805e3cf71b37dd303c13f0c7795
Anton Dyshkant <vyshkant@gmail.com>
2022-07-04 08:21:29 +0400

    Interface fixes

--- src/dotnet_nlp/rule_engine/bundle/helpers.py
+++ src/dotnet_nlp/rule_engine/bundle/helpers.py
@@ -28,4 +28,4 @@ def initialize_and_load():
 def convert_dictionaries(dictionary: dict):
     from System.Collections.Generic import Dictionary, KeyValuePair
 
-    return Dictionary[str, str]({KeyValuePair[str, str](key, value) for key, value in dictionary})
+    return Dictionary[str, str]([KeyValuePair[str, str](key, value) for key, value in dictionary])
