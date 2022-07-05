b9c4bf1af8958d44dbec46c87e3c890fa92aef6c
Anton Dyshkant <vyshkant@gmail.com>
2022-07-05 04:22:35 +0400

    Wrappers edits

--- src/dotnet_nlp/rule_engine/bundle/converter.py
+++ src/dotnet_nlp/rule_engine/bundle/converter.py
@@ -1,19 +1,23 @@
 from typing import Dict, List
-from System import Array
+from System import Array, Object
 from System.Collections.Generic import Dictionary, KeyValuePair
 
 
 class Converter:
     @staticmethod
-    def convert_dictionary__str_to_str(source: Dict[str, str]) -> Dictionary[str, str]:
+    def to_dotnet__dictionary__str_to_str(source: Dict[str, str]) -> Dictionary[str, str]:
         return Dictionary[str, str](
-            Converter.convert_list__kvp_str_str([KeyValuePair[str, str](key, value) for key, value in source.items()])
+            Converter.to_dotnet__list__kvp_str_str([KeyValuePair[str, str](key, value) for key, value in source.items()])
         )
 
     @staticmethod
-    def convert_list__str(source: List[str]) -> Array[str]:
+    def to_dotnet__list__str(source: List[str]) -> Array[str]:
         return Array[str](source)
 
     @staticmethod
-    def convert_list__kvp_str_str(source: List[KeyValuePair[str, str]]) -> Array[str]:
+    def to_dotnet__list__kvp_str_str(source: List[KeyValuePair[str, str]]) -> Array[str]:
         return Array[KeyValuePair[str, str]](source)
+
+    @staticmethod
+    def to_python__dictionary__str_object(source: Dictionary[str, Object]) -> Dict[str, object]:
+        return {pair.Key: pair.Value for pair in source}
--- src/dotnet_nlp/rule_engine/bundle/wrappers.py
+++ src/dotnet_nlp/rule_engine/bundle/wrappers.py
@@ -1,10 +1,10 @@
-from typing import Optional, Dict
+from typing import Optional, Dict, Set
 from DotnetNlp.RuleEngine.Core.Evaluation.Cache import IRuleSpaceCache
 import DotnetNlp.RuleEngine.Core.Evaluation.Rule
 from DotnetNlp.RuleEngine.Core.Evaluation.Rule import IRuleMatcher
 from DotnetNlp.RuleEngine.Core.Evaluation.Rule.Projection.Arguments import RuleArguments
-from DotnetNlp.RuleEngine.Core.Evaluation.Rule.Result import RuleMatchResultCollection
-from DotnetNlp.RuleEngine.Bundle import Factory
+from DotnetNlp.RuleEngine.Core.Evaluation.Rule.Result import RuleMatchResult, RuleMatchResultCollection
+from DotnetNlp.RuleEngine.Bundle import Factory, Strategy
 from dotnet_nlp.rule_engine.bundle.converter import Converter
 
 
@@ -20,7 +20,7 @@ class RuleMatcherWrapper:
             cache: Optional[IRuleSpaceCache] = None
     ) -> RuleMatchResultCollection:
         return self.source.HasMatch(
-            Converter.convert_list__str(phrase.split(' ')),
+            Converter.to_dotnet__list__str(phrase.split(' ')),
             first_symbol_index,
             rule_arguments,
             cache
@@ -34,7 +34,7 @@ class RuleMatcherWrapper:
             cache: Optional[IRuleSpaceCache] = None
     ) -> RuleMatchResultCollection:
         return self.source.HasAnyMatch(
-            Converter.convert_list__str(phrase.split(' ')),
+            Converter.to_dotnet__list__str(phrase.split(' ')),
             first_symbol_index,
             rule_arguments,
             cache
@@ -48,7 +48,7 @@ class RuleMatcherWrapper:
             cache: Optional[IRuleSpaceCache] = None
     ) -> RuleMatchResultCollection:
         return self.source.MatchAndProject(
-            Converter.convert_list__str(phrase.split(' ')),
+            Converter.to_dotnet__list__str(phrase.split(' ')),
             first_symbol_index,
             rule_arguments,
             cache
@@ -62,7 +62,7 @@ class RuleMatcherWrapper:
             cache: Optional[IRuleSpaceCache] = None
     ) -> RuleMatchResultCollection:
         return self.source.MatchAndProjectAll(
-            Converter.convert_list__str(phrase.split(' ')),
+            Converter.to_dotnet__list__str(phrase.split(' ')),
             first_symbol_index,
             rule_arguments,
             cache
@@ -73,8 +73,28 @@ class RuleSpaceWrapper(Dict[str, RuleMatcherWrapper]):
     def __init__(self, rule_sets: Dict[str, str], rules: Dict[str, str]):
         def create():
             return Factory.Create(
-                ruleSets=Converter.convert_dictionary__str_to_str(rule_sets),
-                rules=Converter.convert_dictionary__str_to_str(rules)
+                ruleSets=Converter.to_dotnet__dictionary__str_to_str(rule_sets),
+                rules=Converter.to_dotnet__dictionary__str_to_str(rules)
             )
 
         super().__init__({pair.Key: RuleMatcherWrapper(pair.Value) for pair in create()})
+
+
+class RuleMatchResultWrapper:
+    def __init__(self, rule_match_result: RuleMatchResult):
+        self.source = ' '.join([symbol for symbol in rule_match_result.Source])
+        self.first_used_symbol_index = rule_match_result.FirstUsedSymbolIndex
+        self.last_used_symbol_index = rule_match_result.LastUsedSymbolIndex
+        self.captured_variables = Converter.to_python__dictionary__str_object(rule_match_result.CapturedVariables) if rule_match_result.CapturedVariables is not None else None
+        self.explicitly_matched_symbols_count = rule_match_result.ExplicitlyMatchedSymbolsCount
+        self.marker = rule_match_result.Marker
+        self.lazy_result = rule_match_result.Result
+
+    def get_result(self):
+        return self.lazy_result.Value
+
+
+class RuleMatchResultCollectionWrapper(Set[RuleMatchResultWrapper]):
+    def __init__(self, rule_match_result_collection: RuleMatchResultCollection):
+        super().__init__({RuleMatchResultWrapper(rule_match) for rule_match in rule_match_result_collection})
+        self.best = RuleMatchResultWrapper(rule_match_result_collection.Best(Strategy.Default))
